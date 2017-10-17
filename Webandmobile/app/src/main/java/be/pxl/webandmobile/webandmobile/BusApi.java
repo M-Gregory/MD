4a7b3e34a0e6c75c06f4d703401b8eb89bd4f388
M-Gregory <gregory.malomgre@student.pxl.be>
2017-10-17 12:15:53 +0200

    Testviews voor delijn api zijn af, tijdelijke testdata

--- Webandmobile/app/src/main/java/be/pxl/webandmobile/webandmobile/BusApi.java
+++ Webandmobile/app/src/main/java/be/pxl/webandmobile/webandmobile/BusApi.java
@@ -1,14 +1,35 @@
 package be.pxl.webandmobile.webandmobile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class BusApi extends AppCompatActivity {
+
+    private ArrayList<String> list = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_api);
+
+        Button b = (Button)findViewById(R.id.selectBusStop);
+        Intent intent = new Intent(BusApi.this, TempBusShow.class);
+        intent.putStringArrayListExtra("passedStringArray", list);
+
+        b.setOnClickListener(view->{
+            list.clear();
+            list.add("1");
+            list.add("row 2");
+            list.add("row 3");
+
+            list.add( (((EditText)findViewById(R.id.editableText)).getText()).toString() );
+                    startActivity(intent);
+                });//pass list...
     }
 }
