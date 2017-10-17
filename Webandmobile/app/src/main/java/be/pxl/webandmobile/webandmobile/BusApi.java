f48f54a9901685e86f5badc0ece3a115301e7542
M-Gregory <gregory.malomgre@student.pxl.be>
2017-10-17 11:08:11 +0200

    Bus api selector view opgebouwd, crashfix bij view

--- Webandmobile/app/src/main/java/be/pxl/webandmobile/webandmobile/BusApi.java
+++ Webandmobile/app/src/main/java/be/pxl/webandmobile/webandmobile/BusApi.java
@@ -10,8 +10,5 @@ public class BusApi extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bus_api);
-
-        TextView tv = (TextView) findViewById(R.id.busTextView);
-        //tv.setText("edited text");
     }
 }
